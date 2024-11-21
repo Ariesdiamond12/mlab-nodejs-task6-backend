1f64de6036b6bba3297cdfc5ac14c6a65b7f8835
Precious <92827167+Ariesdiamond12@users.noreply.github.com>
2024-11-21 11:35:50 +0200

    Updated the code

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const router = require("./routes/auth");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT;
 
 // CORS configuration
 const corsOptions = {
-  origin: "https://mlab-nodejs-task8-server.onrender.com", // Update with your frontend's URL if different
+  origin: "https://m-lab-employee-app-1wio.vercel.app", // Update with your frontend's URL if different
   methods: "GET,POST,PUT,DELETE", // Allowed methods
   allowedHeaders: "Content-Type,Authorization", // Allowed headers
 };
